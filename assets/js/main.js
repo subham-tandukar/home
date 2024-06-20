d19e84c49ac4a25a205208843f12ebc085fb2c63
Subham <subhamtndkr9@gmail.com>
2024-06-21 01:33:11 +0545

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,6 @@
 var $ = jQuery;
 
-//for animation on scrolling effects
-AOS.init({
-  once: true,
-});
 
-// Fancybox Config
-Fancybox.bind('[data-fancybox="gallery"]', {
-  //
-});
 
 headerScroll = () => {
   $(window).scroll(function () {
@@ -70,24 +62,192 @@ bannerCarousel = () => {
     items: 1,
     loop: true,
     dots: false,
-    nav: true,
+    smartSpeed: 2000,
+    // nav: true,
     autoplay: true,
     autoplayTimeout: 10000,
-    navText: [
-      "<div class='prev-slide custom__nav'><span></span></div>",
-      "<div class='next-slide custom__nav'><span></span></div>",
-    ],
+    mouseDrag: false,
+    animateOut: "fadeOut",
+    // navText: [
+    //   "<div class='prev-slide custom__nav'><span></span></div>",
+    //   "<div class='next-slide custom__nav'><span></span></div>",
+    // ],
+  });
+};
+reviewCarousel = () => {
+  $(".review__carousel .owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    smartSpeed: 2000,
+    margin: 70,
+    // nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    // navText: [
+    //   "<div class='prev-slide custom__nav'><span></span></div>",
+    //   "<div class='next-slide custom__nav'><span></span></div>",
+    // ],
   });
 };
 
-aboutTab = () => {
-  $(".about__tab__link").click(function () {
-    var tab_id = $(this).attr("data-tab").toLowerCase();
+activitiesCarousel = () => {
+  var mySlider = $(".activities__carousel .owl-carousel");
+  mySlider.owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    nav: false,
+    autoHeight: true,
+    smartSpeed: 1000,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1.25,
+      },
+     
+      960: {
+        items: 1.5,
+      },
+      1200: {
+        items: 2.5,
+      },
+    },
+  });
 
-    $(".about__tab__link").removeClass("current");
-    $(".about__tab__content").removeClass("current");
-    $(this).addClass("current");
-    $("#" + tab_id).addClass("current");
+  $(".activities-arrow .nextarrow").click(function () {
+    mySlider.trigger("next.owl.carousel", [1000]);
+    toggleMyArrows();
+  });
+
+  $(".activities-arrow .prevarrow").click(function () {
+    mySlider.trigger("prev.owl.carousel", [1000]);
+    toggleMyArrows();
+  });
+
+  $(".owl-dot").click(function () {
+    var dotIndex = $(this).index();
+    mySlider.trigger("to.owl.carousel", [dotIndex, 1000]);
+    toggleMyArrows();
+  });
+
+  // Function to toggle arrows based on the current slide position
+  function toggleMyArrows() {
+    var currentIndex = mySlider.find(".owl-item.active").index();
+    var totalItems = mySlider.find(".owl-item").length;
+
+    if (currentIndex === 0) {
+      $(".activities-arrow .prevarrow").addClass("disabled");
+    } else {
+      $(".activities-arrow .prevarrow").removeClass("disabled");
+    }
+
+    if (currentIndex === totalItems - 1) {
+      $(".activities-arrow .nextarrow").addClass("disabled");
+    } else {
+      $(".activities-arrow .nextarrow").removeClass("disabled");
+    }
+
+    if (totalItems === 1) {
+      $(".activities-arrow .nextarrow").addClass("uk-hidden");
+      $(".activities-arrow .prevarrow").addClass("uk-hidden");
+    } else {
+      $(".activities-arrow .nextarrow").removeClass("uk-hidden");
+      $(".activities-arrow .prevarrow").removeClass("uk-hidden");
+    }
+  }
+
+  // Initial toggle based on initial slide position
+  mySlider.on("changed.owl.carousel", function () {
+    toggleMyArrows();
+  });
+  // Assume that 'mySlider' is your Owl Carousel instance
+  mySlider.on("dragged.owl.carousel", function (event) {
+    // Call the functions on touch drag
+    toggleMyArrows();
+  });
+
+  toggleMyArrows();
+};
+
+scrollAnimation = () => {
+  let options = {
+    threshold: 0.9,
+  };
+  let imgoptions = {
+    threshold: 0.5,
+  };
+
+  const animateHeading = $(".animate__heading");
+  const animateImg = $(".animate__img");
+  const reviewBg = document.querySelector(".review__bg");
+
+  let reviewobserver = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting === true) {
+      reviewBg.classList.add("visible");
+    }
+  }, options);
+  reviewBg && reviewobserver.observe(reviewBg);
+
+  // For animateHeading Loop through each heading --------------
+  animateHeading.each(function (index) {
+    let headingObserver = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting === true) {
+        // Add class to the current .step__grid
+        $(this).addClass("visible");
+      }
+      // else {
+      //   // Remove class from the current .step__grid
+      //   $(this).removeClass("visible");
+      // }
+    }, options);
+
+    // Observe the current .step__grid
+    headingObserver.observe($(this).get(0)); // Get the first element from the jQuery collection
+  });
+
+  // For animateImg Loop through each img --------------
+  animateImg.each(function (index) {
+    let imgObserver = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting === true) {
+        // Add class to the current .step__grid
+        $(this).addClass("visible");
+      }
+      // else {
+      //   // Remove class from the current .step__grid
+      //   $(this).removeClass("visible");
+      // }
+    }, imgoptions);
+
+    // Observe the current .step__grid
+    imgObserver.observe($(this).get(0)); // Get the first element from the jQuery collection
+  });
+};
+
+mouseMove = () => {
+  $(".service__section").on("mousemove", function (event) {
+    const cursor = $(".cursor");
+    const { clientX, clientY } = event;
+    cursor.css({
+      transform: `translate(${clientX}px, ${clientY}px)`,
+    });
+  });
+  $(".swiper-button-next").on("mouseenter", function (event) {
+    const cursor = $(".cursor");
+    cursor.addClass("cursor-right");
+  });
+  $(".swiper-button-next").on("mouseleave", function (event) {
+    const cursor = $(".cursor");
+    cursor.removeClass("cursor-right");
+  });
+  $(".swiper-button-prev").on("mouseenter", function (event) {
+    const cursor = $(".cursor");
+    cursor.addClass("cursor-left");
+  });
+  $(".swiper-button-prev").on("mouseleave", function (event) {
+    const cursor = $(".cursor");
+    cursor.removeClass("cursor-left");
   });
 };
 
@@ -95,5 +255,8 @@ jQuery(document).ready(function ($) {
   headerScroll();
   menuOffcanvas();
   bannerCarousel();
-  aboutTab();
+  reviewCarousel();
+  activitiesCarousel();
+  scrollAnimation();
+  mouseMove();
 });
